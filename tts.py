cc8d387dd1c0fc61eb7006d01ff8a73895a29c3b
Raizudeen S <raizudeen.shameem@sifycorp.com>
2024-10-14 11:33:36 +0530

    verion 1.2

--- tts.py
+++ tts.py
@@ -1,173 +1,47 @@
-from pprint import pprint
-import os
-import sys
-import uuid , time
-import edge_tts
-import ast
-from asyncio import run as async_run
-import asyncio
-audio_responses_directory = "audio_responses"
-if not os.path.exists(audio_responses_directory):
-    os.mkdir(audio_responses_directory)
-current_directory = "./audio_responses"
- 
-male_voice = []
-female_voice = []
-async def voice_list():
-  from edge_tts import VoicesManager
-  voices = await VoicesManager.create()
-  voice = voices.find(Gender="Male", Language="en")
-  for voi in voice:
-    male_voice.append(voi["ShortName"])
-  #pprint(male_voice)
-  voice = voices.find(Gender="Female", Language="en")
-  for voi in voice:
-    female_voice.append(voi["ShortName"])
-  #pprint(female_voice)
- 
-import asyncio
-loop = asyncio.get_event_loop_policy().get_event_loop()
-try:
-   loop.run_until_complete(voice_list())
-finally:
-   loop.close()
-print("Voice list setup completed")
-print(male_voice)
- 
-STYLE = """
-body {
-    background-color: #1a1a1a;
-    color: #ffffff;
+import requests
+from pydub import AudioSegment
+import io
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+url = config["URL"]
+
+audio_file_path = "ai.wav"
+male_voice = ['en-AU-WilliamNeural', 'en-CA-LiamNeural', 'en-HK-SamNeural', 'en-IN-PrabhatNeural', 'en-IE-ConnorNeural', 'en-KE-ChilembaNeural', 'en-NZ-MitchellNeural', 'en-NG-AbeoNeural', 'en-PH-JamesNeural', 'en-SG-WayneNeural', 'en-ZA-LukeNeural', 'en-TZ-ElimuNeural', 'en-GB-RyanNeural', 'en-GB-ThomasNeural', 'en-US-AndrewMultilingualNeural', 'en-US-BrianMultilingualNeural', 'en-US-AndrewNeural', 'en-US-BrianNeural', 'en-US-ChristopherNeural', 'en-US-EricNeural', 'en-US-GuyNeural', 'en-US-RogerNeural', 'en-US-SteffanNeural']
+female_voice = ['en-AU-NatashaNeural', 'en-CA-ClaraNeural', 'en-HK-YanNeural', 'en-IN-NeerjaExpressiveNeural', 'en-IN-NeerjaNeural', 'en-IE-EmilyNeural', 'en-KE-AsiliaNeural', 'en-NZ-MollyNeural', 'en-NG-EzinneNeural', 'en-PH-RosaNeural', 'en-SG-LunaNeural', 'en-ZA-LeahNeural', 'en-TZ-ImaniNeural', 'en-GB-LibbyNeural', 'en-GB-MaisieNeural', 'en-GB-SoniaNeural', 'en-US-AvaMultilingualNeural', 'en-US-EmmaMultilingualNeural', 'en-US-AvaNeural', 'en-US-EmmaNeural', 'en-US-AnaNeural', 'en-US-AriaNeural', 'en-US-JennyNeural', 'en-US-MichelleNeural']
+
+
+def TTS_run(text,voice):
+    payload = {
+    "data": [
+        text,
+        voice,
+        None
+    ],
+    "event_data": None,
+    "fn_index": 1
 }
-#prompt-txt > label > span {
-    display: none !important;
-}
-#prompt-txt > label > textarea {
-    border: transparent;
-    box-shadow: none;
-}
-.light-theme #prompt-txt > label > textarea {
-    color: #000000; /* Text color for light theme */
-}
-.dark-theme #prompt-txt > label > textarea {
-    color: #ffffff; /* Text color for dark theme */
-}
- 
-#left-top {
-  padding-left: 0; /* Remove left padding completely */
-  padding-right: 2%;
-  margin-left: 0; /* Remove any left margin */
-  text-align: left; /* Ensure text starts at the left edge */
-  font-weight: bold;
-  font-size: 1.5em;
-  color: #ffffff;
-}
- 
-#aux-btns-popup {
-    position: absolute !important;
-}
-#aux-btns-popup > div {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-}
-.aux-btn {
-    flex: 0 0 98%; /* Adjusted width to be half of the window */
-    margin: 1%; /* Added margin for spacing between buttons */
-    font-weight: unset !important;
-    box-shadow: none !important;
-    border-radius: 20px !important;
-}
-.light-theme .aux-btn {
-    color: #000000; /* Adjust text color for light theme */
-}
-.dark-theme .aux-btn {
-    color: #ffffff; /* Adjust text color for dark theme */
-}
-.aux-btn:hover {
-    box-shadow: 0.3px 0.3px 0.3px gray !important;
-}
-"""
- 
-audio__store_current_directory = 'audio_responses'
-async def gen_audio(text,VoiceId):
-    try:
-        unique_hex_name = f"{int(time.time())}_{uuid.uuid4().hex}"
-        communicate = edge_tts.Communicate(text, VoiceId)
-    except Exception as e:
-        print(Exception)
-    output = os.path.join(audio__store_current_directory, f"{unique_hex_name}.mp3")
-    with open(output, "wb") as file:
-        async for chunk in communicate.stream():
-            if chunk["type"] == "audio":
-                file.write(chunk["data"])
-    return f"{audio__store_current_directory}/{unique_hex_name}.mp3"
- 
-import gradio as gr
-with gr.Blocks(css=STYLE) as kb_chatbot:
-  with gr.Column(scale=1, min_width=180,visible=True) as component1:
-    gr.Markdown("Edge TTS", elem_id="left-top")
-    voice_char = gr.Dropdown(["female_voice","male_voice"],elem_id="chara",label="Select Character",show_label=True)
-    female_voice_char = gr.Dropdown([i for i in female_voice],elem_id="fchara",label="female_voice",show_label=True,visible=False)
-    male_voice_char = gr.Dropdown([j for j in male_voice],elem_id="mchara",label="male_voice",show_label=True,visible=False)
-    instruction_txtbox = gr.Textbox(placeholder="Enter the text", label="",elem_id="prompt-txt")
-    aud = gr.Audio(autoplay=True,visible=True)
- 
-    #Api components
-    with gr.Row():
-      api_data = gr.Textbox(visible=False)
-      api_response = gr.JSON(visible=False)
-      api_send_button = gr.Button(visible=False)
- 
-  async def audio_stream(text,female_voice_char,male_voice_char):
-    try:
-      VoiceId = ""
-      if female_voice_char != None:
-        VoiceId = female_voice_char
-      if male_voice_char != None:
-        VoiceId = male_voice_char
-      unique_hex_name = f"{int(time.time())}_{uuid.uuid4().hex}"
-      communicate = edge_tts.Communicate(text, VoiceId)
-    except Exception as e:
-      print(Exception)
-      sys.exit(-1)
-    output = os.path.join(current_directory, f"{unique_hex_name}.mp3")
-    with open(output, "wb") as file:
-      async for chunk in communicate.stream():
-        if chunk["type"] == "audio":
-          file.write(chunk["data"])
-    return f"{current_directory}/{unique_hex_name}.mp3"
- 
-  def voice_selection(voice_char):
-    if voice_char == "female_voice":
-      return female_voice_char.update(visible=True),male_voice_char.update(visible=False,value=None)
-    if voice_char == "male_voice":
-      return female_voice_char.update(visible=False,value=None),male_voice_char.update(visible=True)
- 
-  def api_respond(api_data):
-    print(api_data)
-    api_data = ast.literal_eval(api_data)
-    message = api_data.get('text')
-    voice_id = api_data.get('voice_id','')
-    if voice_id == '':
-        voice_id = 'en-IN-NeerjaExpressiveNeural'            
-    try:
-      audio_filename = async_run(gen_audio(message,voice_id))
-      print(audio_filename)
-      api_response = {
-          'audio_url': f'http://43.205.228.194/file={os.path.abspath(audio_filename)}'
-      }
-      return api_response
-    except Exception as e:
-      api_response = {
-          'audio_url': f'Failed to generate response'
-      }
-      return api_response
- 
-  voice_char.change(voice_selection,voice_char,[female_voice_char,male_voice_char])
-  instruction_txtbox.submit(audio_stream, inputs=[instruction_txtbox,female_voice_char,male_voice_char], outputs=[aud])
- 
-  api_send_button.click(api_respond, inputs=[api_data], outputs=[api_response],api_name="audio")
- 
-kb_chatbot.launch(share=False, debug=True)
-#kb_chatbot.launch(share=True, debug=True,server_port=8000)
+    response = requests.post(url+"/run/predict", json=payload)
+
+    if response.status_code == 200:
+        try:
+            json_data = response.json() 
+
+            data = json_data.get('data')
+            file_name = url + "/file=" +data[0]['name']
+
+            response = requests.get(file_name)
+
+            if response.status_code == 200:
+                mp3_data = io.BytesIO(response.content)
+                audio = AudioSegment.from_mp3(mp3_data)
+                audio.export(audio_file_path, format="wav")
+                return audio_file_path
+                
+            else:
+                print(f"Failed to download the file. Status code: {response.status_code}")
+
+        except ValueError:
+            print("Response is not in valid JSON format")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
--- ttsnames.py
+++ ttsnames.py
@@ -1,57 +1,14 @@
-import requests
-import json
-from pydub import AudioSegment
-import io
 import gradio as gr
-from dotenv import dotenv_values
+from tts import male_voice,female_voice,TTS_run
 
-config = dotenv_values(".env")
-url = config["URL"]
+male_images = ["inputs/faces/thumbnils/men1.png", "inputs/faces/thumbnils/men2.png", "inputs/faces/thumbnils/men3.png"]
+female_images = ["inputs/faces/thumbnils/women1.png", "inputs/faces/thumbnils/women2.png", "inputs/faces/thumbnils/women3.png"]
 
-audio_file_path = "ai.wav"
-male_voice = ['en-AU-WilliamNeural', 'en-CA-LiamNeural', 'en-HK-SamNeural', 'en-IN-PrabhatNeural', 'en-IE-ConnorNeural', 'en-KE-ChilembaNeural', 'en-NZ-MitchellNeural', 'en-NG-AbeoNeural', 'en-PH-JamesNeural', 'en-SG-WayneNeural', 'en-ZA-LukeNeural', 'en-TZ-ElimuNeural', 'en-GB-RyanNeural', 'en-GB-ThomasNeural', 'en-US-AndrewMultilingualNeural', 'en-US-BrianMultilingualNeural', 'en-US-AndrewNeural', 'en-US-BrianNeural', 'en-US-ChristopherNeural', 'en-US-EricNeural', 'en-US-GuyNeural', 'en-US-RogerNeural', 'en-US-SteffanNeural']
-female_voice = ['en-AU-NatashaNeural', 'en-CA-ClaraNeural', 'en-HK-YanNeural', 'en-IN-NeerjaExpressiveNeural', 'en-IN-NeerjaNeural', 'en-IE-EmilyNeural', 'en-KE-AsiliaNeural', 'en-NZ-MollyNeural', 'en-NG-EzinneNeural', 'en-PH-RosaNeural', 'en-SG-LunaNeural', 'en-ZA-LeahNeural', 'en-TZ-ImaniNeural', 'en-GB-LibbyNeural', 'en-GB-MaisieNeural', 'en-GB-SoniaNeural', 'en-US-AvaMultilingualNeural', 'en-US-EmmaMultilingualNeural', 'en-US-AvaNeural', 'en-US-EmmaNeural', 'en-US-AnaNeural', 'en-US-AriaNeural', 'en-US-JennyNeural', 'en-US-MichelleNeural']
-
-def update_voice_list(gender):
+def update_previews(gender):
     if gender == "Male":
-        return gr.update(choices=male_voice,visible=True)
+        return male_images, gr.update(choices=male_voice,visible=True)
     elif gender == "Female":
-        return gr.update(choices=female_voice,visible=True)
-
-def TTS_run(text,voice):
-    payload = {
-    "data": [
-        text,
-        voice,
-        None
-    ],
-    "event_data": None,
-    "fn_index": 1
-}
-    response = requests.post(url+"/run/predict", json=payload)
-
-    if response.status_code == 200:
-        try:
-            json_data = response.json() 
-
-            data = json_data.get('data')
-            file_name = url + "/file=" +data[0]['name']
-
-            response = requests.get(file_name)
-
-            if response.status_code == 200:
-                mp3_data = io.BytesIO(response.content)
-                audio = AudioSegment.from_mp3(mp3_data)
-                audio.export(audio_file_path, format="wav")
-                return audio_file_path
-                
-            else:
-                print(f"Failed to download the file. Status code: {response.status_code}")
-
-        except ValueError:
-            print("Response is not in valid JSON format")
-    else:
-        print(f"Request failed with status code: {response.status_code}")
+        return female_images, gr.update(choices=female_voice,visible=True)
 
 def create_interface():
     with gr.Blocks() as demo:
@@ -65,7 +22,12 @@ def create_interface():
                     gender_input = gr.Dropdown(choices=["Male", "Female"], label="Gender", interactive=True)
                     voice_dropdown = gr.Dropdown(label="Voice", interactive=True)
                 # Set up dependency between gender and voice dropdown
-                gender_input.change(update_voice_list, inputs=gender_input, outputs=voice_dropdown)
+                with gr.Row():
+                    image_gallery = gr.Gallery(label="Image Previews", interactive=True,columns=3)
+                    gender_input.change(fn=update_previews, inputs=gender_input, outputs=[image_gallery,voice_dropdown])
+
+                video_gen_location = gr.Radio(label="Video Generation Location", choices=["Top Rigth","Top Left","Bottom Right", "Bottom Left"], value="Top Right",interactive=True)
+
             with gr.Column():
 
                 # Interface for TTS
@@ -77,6 +39,6 @@ def create_interface():
                 submit_button.click(TTS_run, inputs=[text_input, voice_dropdown], outputs=output)
 
     # Launch the interface
-    demo.launch()
+    demo.launch(debug=True)
 
 create_interface()
